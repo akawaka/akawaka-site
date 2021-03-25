a64491ffea7f1a48cc8e49307273b89fbf3c1dbc
Alexandre Balmes <alexandre@vanoix.com>
2021-03-25 23:12:01 +0100

    Add static controller and mimic main version of this website

--- webpack.config.js
+++ webpack.config.js
@@ -5,57 +5,27 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
-  // directory where compiled assets will be stored
   .setOutputPath('public/build/front/')
-  // public path used by the web server to access the output path
   .setPublicPath('/build/front')
   // only needed for CDN's or sub-directory deploy
   //.setManifestKeyPrefix('build/')
 
-  /*
-   * ENTRY CONFIG
-   *
-   * Add 1 entry for each "page" of your app
-   * (including one that's included on every page - e.g. "app")
-   *
-   * Each entry will result in one JavaScript file (e.g. app.js)
-   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-   */
-  .addEntry('app', './assets/front/app.js')
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+  .addEntry('front', './assets/front/app.js')
   .splitEntryChunks()
-
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
   .enableSingleRuntimeChunk()
-
-  /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
-   */
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
   .enableSourceMaps(!Encore.isProduction())
-  // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(Encore.isProduction())
-
   .configureBabel((config) => {
     config.plugins.push('@babel/plugin-proposal-class-properties');
   })
-
-  // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
     config.useBuiltIns = 'usage';
     config.corejs = 3;
   })
-
-  // enables Sass/SCSS support
   .enableSassLoader()
-
+  .enablePostCssLoader()
   // uncomment if you use TypeScript
   //.enableTypeScriptLoader()
 
@@ -68,12 +38,48 @@ Encore
 
   // uncomment if you're having problems with a jQuery plugin
   //.autoProvidejQuery()
-
-  // enable PostCSS support
-  .enablePostCssLoader()
 ;
 
 const frontConfig = Encore.getWebpackConfig();
 Encore.reset();
 
-module.exports = [frontConfig];
+Encore
+  .setOutputPath('public/build/admin/')
+  .setPublicPath('/build/admin')
+  // only needed for CDN's or sub-directory deploy
+  //.setManifestKeyPrefix('build/')
+
+  .addEntry('front', './assets/admin/app.js')
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabel((config) => {
+    config.plugins.push('@babel/plugin-proposal-class-properties');
+  })
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = 'usage';
+    config.corejs = 3;
+  })
+  .enableSassLoader()
+  .enablePostCssLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
+
+// uncomment if you use React
+//.enableReactPreset()
+
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
+
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
+;
+
+const adminConfig = Encore.getWebpackConfig();
+Encore.reset();
+
+module.exports = [frontConfig, adminConfig];
